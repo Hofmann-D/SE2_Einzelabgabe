ad2d070d1f16a6fbd3bcb20107be93c52b381b88
Daniel <hofmann-d@gmx.at>
2019-03-20 12:42:13 +0100

    connected Button, EditText and TextView with findViewByID() Method to code

--- app/src/main/java/at/aau/MainActivity.java
+++ app/src/main/java/at/aau/MainActivity.java
@@ -2,14 +2,25 @@ package at.aau;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText tv;
+    TextView tv2;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("NetzwerkTest");
+
+        tv = findViewById(R.id.matrikelnummer);
+        tv2 = findViewById(R.id.labelServerMessage);
+        btn1 = findViewById(R.id.btn);
 
     }
 }
